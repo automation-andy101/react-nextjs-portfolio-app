27affe0ca6a4c389492704d4e0a4f91f02fb7fd4
Andy Short <automation.andy101@gmail.com>
2023-04-12 21:49:34 +0100

    Added about me sections

--- src/components/TimeLine/TimeLine.js
+++ src/components/TimeLine/TimeLine.js
@@ -10,9 +10,9 @@ const Timeline = () => {
   const [activeItem, setActiveItem] = useState(0);
   const carouselRef = useRef();
 
-  // const scroll = (node, left) => {
-  //   return node.scrollTo({ left, behavior: 'smooth' });
-  // }
+  const scroll = (node, left) => {
+    return node.scrollTo({ left, behavior: 'smooth' });
+  }
 
   const handleClick = (e, i) => {
     e.preventDefault();
@@ -125,6 +125,7 @@ const Timeline = () => {
           ))
         }
       </CarouselButtons>
+      <SectionDivider />
     </Section>
   );
 };
