79604069be64a323c6cdf14fddaa771e1b7cb1ff
Andy Short <automation.andy101@gmail.com>
2023-04-13 06:26:32 +0100

    Added accomplishments sections

--- src/components/Acomplishments/Acomplishments.js
+++ src/components/Acomplishments/Acomplishments.js
@@ -7,13 +7,21 @@ const data = [
   { number: 20, text: 'Open Source Projects'},
   { number: 1000, text: 'Students', },
   { number: 1900, text: 'Github Followers', },
-  { number: 5000, text: 'Github Stars', }
+  { number: '40+', text: 'Github Projects', }
 ];
 
 const Acomplishments = () => (
-  <div>
-    Acomplishments
-  </div>
+  <Section>
+    <SectionTitle>Personal Accompliments</SectionTitle>
+    <Boxes>
+      {data.map((card, index) => (
+        <Box key={index}>
+          <BoxNum>{card.number}+</BoxNum>
+          <BoxNum>{card.text}</BoxNum>
+        </Box>
+      ))}
+    </Boxes>
+  </Section>
 );
 
 export default Acomplishments;
--- src/constants/constants.js
+++ src/constants/constants.js
@@ -92,9 +92,7 @@ export const projects = [
 ];
 
 export const TimeLineData = [
-  { year: 2017, text: 'Started my journey', },
-  { year: 2018, text: 'Worked as a freelance developer', },
-  { year: 2019, text: 'Founded JavaScript Mastery', },
-  { year: 2020, text: 'Shared my projects with the world', },
-  { year: 2021, text: 'Started my own platform', },
+  { year: '2015-2019', text: 'Ultra Elctronics Airport Systems - Software Tester', },
+  { year: '2019-2022', text: 'ADB Safegate Airport Systems - Software Tester', },
+  { year: '2022-2023', text: 'Founded JavaScript Mastery', }
 ];
